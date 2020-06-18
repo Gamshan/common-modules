28c0de9b81ede2e9bd4c5de43254e47a7eaee9cd
gamsh <gamshan001@gmail.com>
2020-06-19 00:12:03 +0530

    Auth file refactoring

--- src/common/Auth.js
+++ src/common/Auth.js
@@ -1,44 +1,50 @@
+import {getSafeValues} from "./Functions";
+
 class Auth {
 
+    static  appName;
+
+
     static saveToken (token) {
-        localStorage.setItem('wavy-token', JSON.stringify(token))
+        localStorage.setItem(this.appName + '-token', JSON.stringify(token))
     }
 
     static getToken () {
-        return JSON.parse(localStorage.getItem('wavy-token'))
+        return JSON.parse(localStorage.getItem(this.appName + '-token'))
     }
 
     static saveUser (user) {
-        localStorage.setItem('wavy-user', JSON.stringify(user))
+        localStorage.setItem(this.appName + '-user', JSON.stringify(user))
     }
 
     static getUser () {
-        return JSON.parse(localStorage.getItem('wavy-user'))
+        return JSON.parse(localStorage.getItem(this.appName + '-user'))
     }
 
     static isAccessToken () {
-        const token = JSON.parse(localStorage.getItem('wavy-token'))
+        const token = JSON.parse(localStorage.getItem(this.appName + '-token'))
         return !!token ? !!token : false
     }
 
     static getRoles () {
-        const user = JSON.parse(localStorage.getItem('wavy-user'))
+        const user = JSON.parse(localStorage.getItem(this.appName + '-user'))
         return !!user && !!user.roles ? user.roles : []
     }
 
     static getUserType () {
-        const user = JSON.parse(localStorage.getItem('wavy-user'))
+        const user = JSON.parse(localStorage.getItem(this.appName+'-user'))
         return !!user && !!user.userType ? user.userType : ''
     }
 
     static clearAll () {
-        localStorage.removeItem('wavy-token')
-        localStorage.removeItem('wavy-user')
+        localStorage.removeItem(this.appName+'-token')
+        localStorage.removeItem(this.appName+'-user')
     }
 
 
     static getUserId () {
-        const user = JSON.parse(localStorage.getItem('wavy-user'))
+        console.log(this.appName,'  sssssss')
+        const user = JSON.parse(localStorage.getItem(this.appName+'-user'))
         return !!user && !!user._id ? user._id : ''
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import Auth from "./common/Auth";
 
-
+Auth.appName = "common-modules"
 
 ReactDOM.render(<div>hello</div>, document.getElementById('root'));
