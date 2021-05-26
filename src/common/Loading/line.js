65182546d92d1b2b9509f92129871c2be038ce93
gamsh <gamshan001@gmail.com>
2021-05-26 21:46:25 +0530

    loading line css

--- src/common/Loading/line.js
+++ src/common/Loading/line.js
@@ -1,6 +1,41 @@
 import React from 'react'
-import './line.css'
+import styled from 'styled-components';
+import _ from 'lodash'
+const LoadingLineCss = styled.div`
+   & {
+    height: ${props => _.get(props,'width',"4px")};
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+    background-color: #ddd;
+    }
+    &:before{
+    display: block;
+    position: absolute;
+    content: "";
+    left: -200px;
+    width: 200px;
+    height: 4px;
+    background-color: ${props => _.get(props,'color',"#1877b4")};
+    animation: loading 2s linear infinite;
+}
 
-const LoadingLine = () => <div className="loader"/>
+@keyframes loading {
+    from {left: -200px; width: 30%;}
+    50% {width: 30%;}
+    70% {width: 70%;}
+    80% { left: 50%;}
+    95% {left: 120%;}
+    to {left: 100%;}
+}
+`;
+
+const LoadingLine = ({color,height}) => {
+    return (
+        <div>
+            <LoadingLineCss color={color} height={height}/>
+        </div>
+    );
+};
 
 export default LoadingLine
