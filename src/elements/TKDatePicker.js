9612834cd055f665727165a02dd9a9b77ce3cb3b
gamsh <gamshan001@gmail.com>
2022-03-12 10:45:38 +0530

    datepicker fixes

--- src/elements/TKDatePicker.js
+++ src/elements/TKDatePicker.js
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import DatePicker from "react-datepicker";
 import moment from 'moment'
 import "react-datepicker/dist/react-datepicker.css"
 
 const TKDatePicker = (props) => {
 
+    const {refer, label, min, max, placeholder,handleOnChange,value,dateFormat,returnFormat} = props
 
 
-        return (
+    const [date, setDate] = useState(value ? new Date(value) : new Date());
+
+    useEffect(()=>{
+        handleCalendarClose()
+    },[])
+
+    useEffect(()=>{
+        if(value)
+            setDate(new Date(value))
+    },[value])
+
+
+    const initialProps = {
+        dateFormat :  dateFormat ? dateFormat : 'yyyy-MM-dd',
+    }
+
+    const  handleDateSelect = (val) => {
+    };
+
+    const  handleCalendarClose = () => {
+        const returnValue = returnFormat && moment(date).format(returnFormat) ?
+            moment(date).format(returnFormat) : date
+        handleOnChange(returnValue,refer)
+    };
+
+    const handleDateChange = (val) =>{
+        setDate(val)
+    };
+
+
+
+    return (
             <div>
-                {/*{label &&*/}
-                    <label htmlFor="sel1" style={{fontSize:'14px'}}>{'label xx'}</label>
-                {/*}*/}
+                {label &&
+                    <label htmlFor="sel1" style={{fontSize:'14px'}}>{'label OOO xx'}</label>
+                }
                 <div>
                     {/*<DatePicker*/}
                     {/*    ref={myRef}*/}
